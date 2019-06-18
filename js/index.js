e8c7dc98e6f1dd9cd455efe1f1870cb3a1df985e
masonkarsevar <masonkarsevar@gmail.com>
2019-06-18 11:54:05 -0700

    2 new event handlers on navbar WIP: click events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+
+// === Click and mouseenter and mouseleave events for nav bar
+const navBars = document.querySelectorAll('.nav-link');
+
+navBars.forEach(navBar => navBar.addEventListener('mouseenter', event => {
+	event.target.style.color = '#17A2B8'
+})) 
+
+navBars.forEach(navBar => navBar.addEventListener('mouseleave', event => {
+	event.target.style.color = '#212529'
+}))
+
+// Click event idea for navigation elements:
+const bodySelector = document.querySelector('body');
+
+navBars.forEach(navBar => navBar.addEventListener('click', event => {
+
+}))
+
